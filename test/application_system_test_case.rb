afd4541087fdd12b21fd933e1a103200ecda32b0
Brian-Demon <BrianWMatejka@gmail.com>
2021-04-07 18:18:53 -0500

    Attempt fix on Selenium

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,7 +4,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1600, 1080], options: {
     url: "http://localhost:4444/wd/hub",
     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w[headless window-size=1600x1080] },
+      chromeOptions: { args: %w[headless window-size=1600x1080 disable-gpu no-sandbox disable-dev-shm-usage] },
     )
   }
 
