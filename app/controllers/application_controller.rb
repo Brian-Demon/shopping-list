a240f44ec489d8797f2ed6574fcea2e0141f2b1f
Brian-Demon <BrianWMatejka@gmail.com>
2021-04-20 14:10:53 -0500

    fix when no User in the databse on first viewing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
   private
  
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if User.count > 0
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
   end
 end
