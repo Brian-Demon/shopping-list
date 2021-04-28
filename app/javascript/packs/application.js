5c47e8a3fd231cb44f957a82f488f4ed91658e9e
Brian-Demon <BrianWMatejka@gmail.com>
2021-04-28 18:03:42 -0500

    Initial

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,3 +32,7 @@ window.addEventListener("load", () => {
         });
     })
 });
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
