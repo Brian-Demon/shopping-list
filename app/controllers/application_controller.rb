0ac010c017cb123e5299ae8f40953e5e386c4026
Brian-Demon <BrianWMatejka@gmail.com>
2021-05-10 15:03:16 -0500

    Fix no users in db when getting current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   private
  
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] unless User.count == 0
   end
 end
