6a921ec239cc9d93a3520830fc34c88b980d47e8
Brian-Demon <BrianWMatejka@gmail.com>
2021-05-11 19:46:18 -0500

    Update app/controllers/application_controller.rb
    Co-authored-by: Andrew Nordman <cadwallion@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   private
  
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id] unless User.count == 0
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 end
