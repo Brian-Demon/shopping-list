ddf8655a45157bce38093d4bf56a6488e03a7347
Brian-Demon <BrianWMatejka@gmail.com>
2021-06-17 19:39:59 -0500

    determine if list is shared for displaying

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -9,10 +9,26 @@ module ListsHelper
       "aria-controls" => list.id 
     }
     link_to("#list-#{list.id}", options) do
-      list_name = content_tag :div, list.name, class: "me-auto list-name"
+      if is_shared_list?(list, current_user)
+        list_name = content_tag :div, list.name + " (shared)", class: "me-auto list-name"
+      else
+        list_name = content_tag :div, list.name, class: "me-auto list-name"
+      end
       item_count = content_tag :span, list.items.active.count, class: "badge bg-primary rounded-pill item-count"
 
       list_name + item_count
     end
+  end
+
+  def is_shared_list?(list, user)
+    if SharedList.find_by(list_id: list.id)
+      true
+    else
+      false
+    end
+  end
+
+  def shared_list_reference(shared_list)
+    List.find_by(id: shared_list.list_id)
   end
 end
