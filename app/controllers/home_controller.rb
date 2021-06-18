852b4d561fdbfe49235cb261554392c907333df2
Brian-Demon <BrianWMatejka@gmail.com>
2021-06-17 19:38:42 -0500

    add @all_lists array to home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
     @previous_item_data = Item.previous_item_data current_user
+    @all_lists = []
+    current_user.lists.each do |list|
+      @all_lists << list
+    end
+    current_user.shared_lists.each do |shared_list|
+      if list = List.find_by(id: shared_list.list_id)
+        @all_lists << list
+      end
+    end
   end
 end
