4210fde586cfe6f8145845ec8d6bdc9f58d8c9b8
Brian-Demon <BrianWMatejka@gmail.com>
2021-06-18 11:44:31 -0500

    refactor `@all_user`
    Made sure `@all_lists` is always defined since there is no conditional for `if current_user` in the list partial (`_lists.html.erb`)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,10 @@
 class HomeController < ApplicationController
   def index
     @previous_item_data = Item.previous_item_data current_user
-    @all_lists = []
     if current_user
-      current_user.lists.each do |list|
-        @all_lists << list
-      end
-      current_user.shared_lists.each do |shared_list|
-        if list = List.find_by(id: shared_list.list_id)
-          @all_lists << list
-        end
-      end
+      @all_lists = current_user.lists + current_user.user_shared_lists
+    else
+      @all_lists = []
     end
   end
 end
