bd36bd68acbd426e7b2fdcb13819b04b198f67bf
Brian-Demon <BrianWMatejka@gmail.com>
2021-08-23 17:10:15 -0500

    when creating user email column is now populated

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
 
   def self.create_with_omniauth(auth)
     create! do |user|
+      user.email = auth["info"]["email"]
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.username = auth["info"]["name"]
